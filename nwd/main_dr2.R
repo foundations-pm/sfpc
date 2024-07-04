1435e8b6a9ce5ef59f7c7ee7f99f32746b22223b
Perrine Machuel <perrine.machuel@foundations.org.uk>
2024-07-04 13:50:33 +0100

    Start dr2 cleaning: data formatting widely differs more info needed

--- nwd/main_dr2.R
+++ nwd/main_dr2.R
@@ -1,4 +1,4 @@
-# Data Cleaning Script for No Wrong Doors RCT DR1 ----
+# Data Cleaning Script for No Wrong Doors RCT dr2 ----
 
 # Paths  ----
 user_directory = 'C:/Users/PerrineMachuel/'
@@ -25,25 +25,26 @@ setwd(data_path)
 # Load data:
 
 # Create list of file names
-dr1_names <- list.files("NWD_DR1", # Name of folder with DR1 data
+dr2_names <- list.files("NWD_DR2", # Name of folder with dr2 data
                         pattern="*.xlsx", full.names=TRUE)
+
 # Remove incomplete files
-dr1_names = dr1_names[-16] # Rochdale Nov 2020
+#dr2_names = dr2_names[-16] # Rochdale Nov 2020
 
 # Read files into local env
-dr1_list <- lapply(dr1_names, function(file) {  
+dr2_list <- lapply(dr2_names, function(file) {  
   print(file) 
-  readxl::read_excel(file, sheet = "DR1 - Data - aggregate level")})
+  readxl::read_excel(file, sheet = "dr2 - Data - aggregate level")})
 
 # Assign names to each file
-string_to_remove = c("NWD_DR1/", ".xlsx")
-dr1_names <- stringr::str_remove_all(dr1_names, 
+string_to_remove = c("NWD_dr2/", ".xlsx")
+dr2_names <- stringr::str_remove_all(dr2_names, 
                                      paste(string_to_remove,
                                            collapse = "|"))
-names(dr1_list) = dr1_names
+names(dr2_list) = dr2_names
 
 # Create LA string
-#la_string = str_extract(dr1_names, ".+?(?=_)")
+#la_string = str_extract(dr2_names, ".+?(?=_)")
 
 # Quality checks ----
 # Key steps:
@@ -52,14 +53,14 @@ names(dr1_list) = dr1_names
 # 3- Missing data 
 # 4- Unique values / unexpected values (e.g., int vs cat)
 
-lapply(1:length(dr1_list), function(i){ 
-  print(names(dr1_list[i]))
-  print(dim(dr1_list[[i]])) 
-  print(sum(is.na(dr1_list[[i]])))})
+lapply(1:length(dr2_list), function(i){ 
+  print(names(dr2_list[i]))
+  print(dim(dr2_list[[i]])) 
+  print(sum(is.na(dr2_list[[i]])))})
 
-#colnames(dr1_list[['NWD_DR1/warrington_dr1_nov20.xlsx']])
-#colnames(dr1_list[['NWD_DR1/warrington_dr1_nov22.xlsx']])
-#colnames(dr1_list[['NWD_DR1/leicester_dr1_nov20.xlsx']])
+#colnames(dr2_list[['NWD_dr2/warrington_dr2_nov20.xlsx']])
+#colnames(dr2_list[['NWD_dr2/warrington_dr2_nov22.xlsx']])
+#colnames(dr2_list[['NWD_dr2/leicester_dr2_nov20.xlsx']])
 
 # Data cleaning ----
 
@@ -102,18 +103,18 @@ lapply(1:length(dr1_list), function(i){
 
 # create clean names for mandatory returns
 mandatory_returns <- janitor::make_clean_names(colnames(
-  dr1_list[['leicester_dr1_apr21']])) 
+  dr2_list[['leicester_dr2_apr21']])) 
 
 # Create merged table
-dr1_data <- purrr::map_dfr(1:length(dr1_list), function(i) {
+dr2_data <- purrr::map_dfr(1:length(dr2_list), function(i) {
   
   # Identify LA return by its name 
   # format = <LA name> _ < month/year of return>
-  name = names(dr1_list)[i] 
+  name = names(dr2_list)[i] 
   
   # Merge all records into one table:
   
-  dr1_list[[i]] <- dr1_list[[i]] %>% 
+  dr2_list[[i]] <- dr2_list[[i]] %>% 
     janitor::clean_names() %>% # Clean column names 
     dplyr::mutate(local_authority = name) %>% # New column with LA identifier
     dplyr::select(local_authority, # select only mandatory returns
@@ -122,16 +123,16 @@ dr1_data <- purrr::map_dfr(1:length(dr1_list), function(i) {
   # temporarily convert all columns as character to bind them together
   
   # check dimension of data
-  print(names(dr1_list[i])) 
-  print(dim(dr1_list[[i]])) 
+  print(names(dr2_list[i])) 
+  print(dim(dr2_list[[i]])) 
   
   # Quality checks 
   # Checks: there should be 12 columns
   # 11 mandatory returns + 1 column for LA return name 
-  if(dim(dr1_list[[i]])[[2]] != 12){ 
+  if(dim(dr2_list[[i]])[[2]] != 12){ 
     print("ISSUE WITH DATA TO CHECK") }
   
-  return(dr1_list[[i]])
+  return(dr2_list[[i]])
   
 })
 
@@ -148,7 +149,7 @@ dr1_data <- purrr::map_dfr(1:length(dr1_list), function(i) {
 # 2021-2022:  (1) Oct 21 - March 22 (Apr 22?); (2) Apr 22 - Sept 22 (Nov22?)
 
 # Assign date class & arrange column conveniently
-dr1_data <- dr1_data %>% 
+dr2_data <- dr2_data %>% 
   dplyr::mutate(month = as.Date(month, "%Y-%m-%d"),
                 month_return = gsub(".*\\_", "", local_authority),
                 month_return = factor(
@@ -158,7 +159,7 @@ dr1_data <- dr1_data %>%
   dplyr::relocate(local_authority, month_return)
 
 # Return date checks 
-returns_date_checks <- dr1_data %>% 
+returns_date_checks <- dr2_data %>% 
   group_by(local_authority, month_return) %>% 
   summarise(total_nb_months = n(), # total number of months returned
             min_month = min(month), # min month
@@ -173,4 +174,4 @@ returns_date_checks %>%
 # Save table
 writexl::write_xlsx(returns_date_checks, 
                     paste0(output_path, 
-                           "dr1_monthly_returns_quality_checks.csv"))
+                           "dr2_monthly_returns_quality_checks.csv"))
