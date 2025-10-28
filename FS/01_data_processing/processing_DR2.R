f8d9f5785b527a06a4dd57420e5a11eeef844b4e
Perrine Machuel <perrine.machuel@foundations.org.uk>
2025-10-28 13:06:14 +0000

    processing DR3

--- FS/01_data_processing/processing_DR2.R
+++ FS/01_data_processing/processing_DR2.R
@@ -1,13 +1,13 @@
-#------------------------------------------------
+#------------------------------------------------#
 #
-# FAMILY SAFEGUARDING: STEPPED WEDGE ANALYSES
+# FAMILY SAFEGUARDING: STEPPED WEDGE ANALYSES ----
 #
-#------------------------------------------------
+#------------------------------------------------#
 
 
-# FAMILY SAFEGUARDING DATA CLEANING: DATA RETURN 2 ----
+## DATA CLEANING: DATA RETURN 2 ----
 
-# Set-up  ----
+## 1. Set-up  ----
 r_directory = 'C:/Users/PerrineMachuel/'
 sharepoint_path = paste0(r_directory,'Foundations/High-SFPC-Impact - Family Safeguarding')
 
@@ -24,9 +24,16 @@ wd = paste0(r_directory, "Documents/SFPC/FS/")
 # Functions 
 { source(paste0(r_directory, "Documents/SFPC/FS/functions.R"))}
 
-# Pre-processing ----
+# Dates 
+date = format(Sys.Date(),"%Y/%m/%d") # date format to save within dataframes
 
-## Load data ----
+file_date = format(Sys.Date(),"_%Y%b%d") # date format to save files
+
+dir_date = format(Sys.Date(),"%B %Y") # date format to create directories
+
+## 2. Pre-processing ----
+
+### 1. Load data ----
 
 # Set working dir to data path
 setwd(data_path)
@@ -57,7 +64,7 @@ names(DR2_list) <- stringr::str_remove_all(
   names(DR2_list),  
   paste(string_to_remove, collapse = "|"))
 
-## Quality checks ----
+### 2. Inspect data ----
 # Key steps:
 # 1- Data set dimensions
 # 2- Column names 
@@ -77,7 +84,7 @@ for(name in names(DR2_list)){
   # Print row 3 to assess where table starts
   print(DR2_list[[name]][[1]][3,]) }
 
-# Step 1: Resize tables ---
+### 3. Resize data ----
 
 # Swindon Nov 22: table starts at row 1 - colnames already correct
 # Resize all tables starting from row 3
@@ -92,7 +99,7 @@ DR2_list[['swindon_dr2_nov22']][[1]] <-  janitor::clean_names(
 lapply(DR2_list, function(data) colnames(data[[1]]))
 lapply(DR2_list, function(data) dim(data[[1]]))
 
-# Step 2: add LA identifier to returns --- 
+### 4. Add LA ID ---- 
 
 # Locate month return and LA
 for(return in names(DR2_list)){ 
@@ -110,7 +117,11 @@ for(return in names(DR2_list)){
         local_authority = local_authority) %>%
       relocate(local_authority, month_return) }
 
-# Step 3: standardise columns across returns ---
+### 5. Standardise columns ----
+
+# Cols to standardise: 
+# (1) Telford Nov 22 needs ethnicity and DOB columns standardised
+# (2) All LAs except Lancashire need cols factors assessed at the end... 1B,2B,3A,3B,3C,4B
 
 # Telford Nov 22 -
 # 1 need to collapse (1) year_and_month_of_birth_of_the_child with (stores month of birth)
@@ -123,7 +134,6 @@ DR2_list[['telford_dr2_nov22']][[1]] = DR2_list[['telford_dr2_nov22']][[1]] %>%
       year_and_month_of_birth_of_the_child, ethnicity, sep ='/')) %>%
   dplyr::select(-ethnicity) %>%
   dplyr::rename(ethnicity = ethnicity_2)
-
 
 # All LAs except Lancashire -
 # factors_identified_at_the_end_of_assessment columns needed for:
@@ -160,28 +170,20 @@ DR2_list[5:20] = lapply(DR2_list[5:20], function(data) { # 1:4 is lancashire
 }) 
 
 # All data is 
-# Bring lancaster to same list level as other LAs
+# Bring Lancaster to same list level as other LAs
 DR2_list[1:4] <- lapply(DR2_list[1:4], `[[`, 1)
 
-# Step 4: unify data class ---
+### 6. Unify data classes ----
 
-# Count number of NAs per column 
-# Make table 
-
-pre_cleaning_missingness = purrr::map_dfr(
-  DR2_list, function(data_return) {
-    dplyr::tibble(
-      column = names(data_return),
-      n_missing = colSums(is.na(data_return))
-    )
-}, .id = 'data_return')
-
+#### 1. As character ----
 # Turn all columns into character class
-DR2_list = lapply(
-  DR2_list, function(data) { data %>% dplyr::mutate(across(everything(), as.character)) })
+DR2_list_transformed = lapply(
+  DR2_list, function(data) { 
+    data %>% dplyr::mutate(across(everything(), as.character)) })
 
+#### 2. As date ----
 # Turn date columns into dates
-DR2_list = lapply(DR2_list, function(data) {
+DR2_list_transformed = lapply(DR2_list_transformed, function(data) {
   
   data %>% 
     dplyr::mutate(
@@ -201,14 +203,35 @@ DR2_list = lapply(DR2_list, function(data) {
         )))
 }) 
 
-# Check missingness post cleaning
-post_cleaning_missingness = purrr::map_dfr(
+
+#### 3. QA ----
+# Compare missingness pre and post cleaning
+# Count number of NAs per column 
+# Make table 
+dr2_pre_cleaning_missingness = purrr::map_dfr(
   DR2_list, function(data_return) {
     dplyr::tibble(
       column = names(data_return),
       n_missing = colSums(is.na(data_return))
     )
   }, .id = 'data_return')
+
+dr2_post_cleaning_missingness = purrr::map_dfr(
+  DR2_list_transformed, function(data_return) {
+    dplyr::tibble(
+      column = names(data_return),
+      n_missing = colSums(is.na(data_return))
+    )
+  }, .id = 'data_return')
+
+# save tables
+tables = c('dr2_pre_cleaning_missingness', 'dr2_post_cleaning_missingness')
+
+mget(tables) %>%                     # fetch objects safely by name
+  iwalk(~ write_xlsx(
+    .x,
+    file.path(output_path, "DR2", "QA", paste0(.y, file_date, ".xlsx"))
+  ))
 
 # discrepancies between DOBs because of unborn children 
 # the rest of NAs are rightfully coded - either NULL, / or N/A values
@@ -217,8 +240,14 @@ post_cleaning_missingness = purrr::map_dfr(
 # some unborn children have no DOB > this will show as NA in year_and_month_of_birth_of_the_child
 # some unborn children have a DOB > but other of their demographic characteristics are coded as 'unborn'
 
-# Step 4: bind rows ---
-DR2_data = do.call(bind_rows, DR2_list)
+### 7. Bind rows ----
+DR2_data = do.call(bind_rows, DR2_list_transformed)
 
-DR2_data = DR2_data %>% dplyr::select(-na, -contains('na_'), -child_birth_month_and_year) %>% colnames()
+DR2_data = DR2_data %>% 
+  dplyr::select(
+  -na, -contains('na_'), -child_birth_month_and_year) 
+
+### 8. Save data ---- 
+saveRDS(DR2_data, file = paste0(
+  output_path,"/DR2/DR2_pre_processed_data.Rds")) 
 
---
+++ FS/01_data_processing/processing_DR3.R
@@ -0,0 +1,170 @@
+#------------------------------------------------#
+#
+# FAMILY SAFEGUARDING: STEPPED WEDGE ANALYSES ----
+#
+#------------------------------------------------#
+
+
+## DATA CLEANING: DATA RETURN 2 ----
+
+## 1. Set-up  ----
+r_directory = 'C:/Users/PerrineMachuel/'
+sharepoint_path = paste0(r_directory,'Foundations/High-SFPC-Impact - Family Safeguarding')
+
+# Data and output paths
+data_path = paste0(sharepoint_path, '/Data')
+output_path = paste0(sharepoint_path, '/Datasets/pre_processing/DR3')
+
+# Working directory
+wd = paste0(r_directory, "Documents/SFPC/FS/")
+
+# Libraries 
+{ source(paste0(r_directory, "Documents/SFPC/FS/config.R")) }
+
+# Functions 
+{ source(paste0(r_directory, "Documents/SFPC/FS/functions.R"))}
+
+# Dates 
+date = format(Sys.Date(),"%Y/%m/%d") # date format to save within dataframes
+
+file_date = format(Sys.Date(),"_%Y%b%d") # date format to save files
+
+dir_date = format(Sys.Date(),"%B %Y") # date format to create directories
+
+## 2. Pre-processing ----
+
+### 1. Load data ----
+
+# Set working dir to data path
+setwd(data_path)
+
+# Load data:
+# Create list of file paths
+DR3_file_paths <- list.files("FS_DR3_2024", 
+                             pattern="*.xlsx",
+                             full.names=TRUE)
+
+# List of all DR2 files
+DR3_list = lapply(DR3_file_paths, function(.file_paths){
+  
+  read_xlsx_worksheets(.file_paths,
+                       sheet_name = 'Data') })
+
+# Assign names to list
+# Clean list names
+names(DR3_list) = DR3_file_paths
+
+string_to_remove = c("FS_DR3_2024/", ".xlsx")
+
+names(DR3_list) <- stringr::str_remove_all(
+  names(DR3_list),  
+  paste(string_to_remove, collapse = "|"))
+
+# Flatten list - remove 1 nesting level
+DR3_list <- lapply(DR3_list, `[[`, 1)
+
+### 2. Inspect data ----
+# Key steps:
+# 1- Data set dimensions
+# 2- Column names 
+# 3- Missing data 
+# 4- Unique values / unexpected values (e.g., int vs cat)
+
+for(name in names(DR3_list)){
+  
+  print(name)
+  # Dimension of data
+  print(dim(DR3_list[[name]])) 
+  
+  # Print head = 5 to assess where table starts
+  print(head(DR3_list[[name]], 5)) }
+
+### 3. Resize data ----
+
+# All tables start at row 4
+# Resize all tables starting from row 4
+DR3_list <- lapply(DR3_list, function(data) {
+  
+  janitor::clean_names( # clean col names to standard names
+    janitor::row_to_names( # make row 1 the columns of each dataset in list
+      data[4:nrow(data),], 1)) # resize dataframe to start from row 4
+  
+})
+
+# Checks
+lapply(DR3_list, function(data) colnames(data))
+lapply(DR3_list, function(data) dim(data))
+
+### 4. Add LA ID ---- 
+
+# Locate month return and LA
+for(data in names(DR3_list)){ 
+  
+  local_authority = str_extract(names(DR3_list[data]), ".+?(?=_)")
+  month_return = gsub(".*\\_", "", names(DR3_list[data]))
+  
+  print(local_authority)
+  print(month_return)
+  
+  # Assign LA name and month return
+  DR3_list[[data]] = DR3_list[[data]] %>% 
+    dplyr::mutate(
+      month_return = month_return,
+      local_authority = local_authority) %>%
+    relocate(local_authority, month_return) }
+
+### 5. Unify data classes ----
+
+# check class for all columns 
+lapply(DR3_list, \(df) sapply(df, class))
+lapply(DR3_list, head)
+
+#### 1. As date ----
+# Turn date columns into dates
+DR3_list_transformed = lapply(DR3_list, function(data) {
+  
+  data %>% 
+    dplyr::mutate(
+      start_date_of_cla_period_of_care_start_date = 
+        janitor::excel_numeric_to_date(
+          as.numeric(start_date_of_cla_period_of_care_start_date))
+      )
+}) 
+
+
+#### 2. QA ----
+# Compare missingness pre and post cleaning
+# Count number of NAs per column 
+# Make table 
+dr3_pre_cleaning_missingness = purrr::map_dfr(
+  DR3_list, function(data_return) {
+    dplyr::tibble(
+      column = names(data_return),
+      n_missing = colSums(is.na(data_return))
+    )
+  }, .id = 'data_return')
+
+dr3_post_cleaning_missingness = purrr::map_dfr(
+  DR3_list_transformed, function(data_return) {
+    dplyr::tibble(
+      column = names(data_return),
+      n_missing = colSums(is.na(data_return))
+    )
+  }, .id = 'data_return')
+
+# save tables
+tables = c('dr3_pre_cleaning_missingness', 'dr3_post_cleaning_missingness')
+
+mget(tables) %>%                     # fetch objects safely by name
+  iwalk(~ write_xlsx(
+    .x,
+    file.path(output_path, "QA", paste0(.y, file_date, ".xlsx"))
+  ))
+
+### 6. Bind rows ----
+DR3_data = do.call(bind_rows, DR3_list_transformed)
+
+### 7. Save data ---- 
+saveRDS(DR3_data, file = paste0(
+  output_path,"/DR3_pre_processed_data.Rds")) 
+
