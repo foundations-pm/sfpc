37a1a2f3a4d127fe412293eaa6bdb4c428158c73
Perrine Machuel <perrine.machuel@foundations.org.uk>
2025-12-28 09:09:26 +0000

    all samples descriptives for report v0

--- FS/03_analyses/primary_outcome/00_samples_descriptives.R
+++ FS/03_analyses/primary_outcome/00_samples_descriptives.R
@@ -70,20 +70,28 @@ overall_desc =  desc_data %>%
   describe(class = 'categorical') %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
-    #count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq)
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq)
+
+overall_desc =  desc_data %>% 
+  describe(class = 'numeric') %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq)
 
 ### Table 1 -----------------------------------------------------------------------
 table_1 =  desc_data %>%
   dplyr::group_by(local_authority, intervention_group) %>%
   dplyr::summarise(count =  n()) %>%
-  dplyr::mutate(freq = round(count/sum(count), 2)) %>%
+  dplyr::mutate(freq = round(`count`/sum(`count`), 2)) %>%
   dplyr::mutate(
-    count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
   dplyr::ungroup() %>%
-  dplyr::select(-count, -freq) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = intervention_group, 
     values_from = count_percent
@@ -104,7 +112,7 @@ table_2 =  desc_data %>%
   dplyr::summarise(count =  n()) %>%
   tidyr::pivot_wider(
     names_from = cla_status, 
-    values_from = count
+    values_from = `count`
   ) %>% 
   dplyr::mutate(
     `Total N` = sum(`0`, `1`),
@@ -161,20 +169,20 @@ writexl::write_xlsx(table_3, 'table_3.xlsx')
 
 ### Appendix tables ----
 
-#### Table 1 ----
+#### Table 1.1 baseline cat ----
 
 desc_data %>% 
   dplyr::group_by(local_authority) %>% 
   dplyr::summarise(n())
 
-appendix_tb_1 =  desc_data %>% 
+appendix_tb_1_cat =  desc_data %>% 
   dplyr::group_by(local_authority) %>%
   describe(class = 'categorical', group = c('local_authority')) %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
-    #count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq) %>%
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = 'local_authority', values_from = 'count_percent') %>%
   dplyr::mutate(
@@ -190,7 +198,71 @@ appendix_tb_1 =  desc_data %>%
 
 # Save table 
 setwd(paste0(output_path, '/1. Main sample/Report descriptive tables'))
-writexl::write_xlsx(appendix_tb_1, 'appendix_table_1.xlsx')
+writexl::write_xlsx(appendix_tb_1_cat, 'appendix_table_1_cat.xlsx')
+
+#### Table 1.2 baseline cont ----
+
+# Numeric vars
+appendix_tb_1_cont =  desc_data %>% 
+  dplyr::select(-age_at_referral_numeric_final) %>%
+  dplyr::group_by(local_authority) %>%
+  describe(class = 'numeric', group = c('local_authority')) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(count_percent_missing = paste0(number_missing, " (", percent_missing, "%)")) %>%
+  dplyr::relocate(c('covariate','local_authority','min', 'max', 'mean', 'sd', 'count_percent_missing')) %>%
+  dplyr::select(-cv,-median,-contains('percentile'), -iqr)
+
+# Save table 
+setwd(paste0(output_path, '/1. Main sample/Report descriptive tables'))
+writexl::write_xlsx(appendix_tb_1_cont, 'appendix_table_1_cont.xlsx')
+
+#### Table 1.3 baseline equivalence ----
+covariates = c(
+  #'referral_no_further_action_clean',
+  'unborn_flag',
+  'age_at_referral_final',
+  'gender_final',
+  'ethnicity_final',
+  'disability_status_clean',
+  'number_of_previous_cpp_clean',
+  'uasc_clean',
+  'prop_white_british')
+
+# create table
+baseline_equivalence_int_group <- tableone::CreateTableOne(
+  vars = covariates,
+  strata = "intervention_group", 
+  data = desc_data, test = FALSE)
+
+# extract SMDs
+smd_equivalence_table = print(baseline_equivalence_int_group, smd = TRUE) 
+smd_equivalence_table = as.data.frame(smd_equivalence_table)
+smd_equivalence_table = tibble::rownames_to_column(
+  smd_equivalence_table, "Covariate")
+# looking like really good baseline equivalence
+
+# Save table
+setwd(paste0(output_path, '/1. Main sample/Report descriptive tables'))
+writexl::write_xlsx(
+  smd_equivalence_table, 
+  'appendix_table_1_baseline_equivalence.xlsx')
+
+# create plot
+formula = paste0("intervention_group ~ ", paste(covariates, collapse = ' + '))
+
+cobalt::bal.tab(
+  as.formula(formula), 
+  data = desc_data,
+  stats = 'm',
+  binary = 'std',
+  abs = TRUE)
+
+cobalt::love.plot(
+  as.formula(formula), 
+  data = desc_data,
+  stats = 'm',
+  binary = 'std',
+  abs = TRUE)
 
 #### Table 2,3,4,5,6 ----------
 
@@ -205,9 +277,9 @@ appendix_tb_234567 =  desc_data %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
     count = 5*round(`count`/5),
-    count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = 'local_authority', values_from = 'count_percent') %>%
   dplyr::mutate(
@@ -273,24 +345,32 @@ desc_data = data %>%
   dplyr::select(covariates)
 
 # Overall description 
-overall_desc =  desc_data %>% 
-  describe(class = 'categorical') %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(
-    #count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq)
+#overall_desc =  desc_data %>% 
+#  describe(class = 'categorical') %>%
+#  dplyr::ungroup() %>%
+#  dplyr::mutate(
+#    #count = ifelse(`count` < 5, '[z]', `count`),
+#    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+#  dplyr::select(-`count`, -freq)
+
+#overall_desc =  desc_data %>% 
+#  describe(class = 'numeric') %>%
+#  dplyr::ungroup() %>%
+#  dplyr::mutate(
+#    #count = ifelse(`count` < 5, '[z]', `count`),
+#    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+#  dplyr::select(-`count`, -freq)
 
 ### Table 1 -----------------------------------------------------------------------
 table_1 =  desc_data %>%
   dplyr::group_by(local_authority, intervention_group) %>%
   dplyr::summarise(count =  n()) %>%
-  dplyr::mutate(freq = round(count/sum(count), 2)) %>%
+  dplyr::mutate(freq = round(`count`/sum(`count`), 2)) %>%
   dplyr::mutate(
-    count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
   dplyr::ungroup() %>%
-  dplyr::select(-count, -freq) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = intervention_group, 
     values_from = count_percent
@@ -311,7 +391,7 @@ table_2 =  desc_data %>%
   dplyr::summarise(count =  n()) %>%
   tidyr::pivot_wider(
     names_from = cla_status, 
-    values_from = count
+    values_from = `count`
   ) %>% 
   dplyr::mutate(
     `Total N` = sum(`0`, `1`),
@@ -368,20 +448,22 @@ writexl::write_xlsx(table_3, 'children_with_risk_factors_table_3.xlsx')
 
 ### Appendix tables ----
 
-#### Table 1 ----
+#### Table 1.1 baseline cat ----
 
 desc_data %>% 
   dplyr::group_by(local_authority) %>% 
   dplyr::summarise(n())
 
-appendix_tb_1 =  desc_data %>% 
+appendix_tb_1_cat =  desc_data %>% 
   dplyr::group_by(local_authority) %>%
   describe(class = 'categorical', group = c('local_authority')) %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
-    #count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq) %>%
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    #count = 5*round(`count`/5),
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = 'local_authority', values_from = 'count_percent') %>%
   dplyr::mutate(
@@ -397,7 +479,71 @@ appendix_tb_1 =  desc_data %>%
 
 # Save table 
 setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
-writexl::write_xlsx(appendix_tb_1, 'children_with_risk_factors_appendix_table_1.xlsx')
+writexl::write_xlsx(appendix_tb_1_cat, 'children_with_risk_factors_appendix_table_1_cat.xlsx')
+
+#### Table 1.2 baseline cont ----
+
+# Numeric vars
+appendix_tb_1_cont =  desc_data %>% 
+  dplyr::select(-age_at_referral_numeric_final) %>%
+  dplyr::group_by(local_authority) %>%
+  describe(class = 'numeric', group = c('local_authority')) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(count_percent_missing = paste0(number_missing, " (", percent_missing, "%)")) %>%
+  dplyr::relocate(c('covariate','local_authority','min', 'max', 'mean', 'sd', 'count_percent_missing')) %>%
+  dplyr::select(-cv,-median,-contains('percentile'), -iqr)
+
+# Save table 
+setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
+writexl::write_xlsx(appendix_tb_1_cont, 'children_with_risk_factors_appendix_table_1_cont.xlsx')
+
+#### Table 1.3 baseline equivalence ----
+covariates = c(
+  #'referral_no_further_action_clean',
+  'unborn_flag',
+  'age_at_referral_final',
+  'gender_final',
+  'ethnicity_final',
+  'disability_status_clean',
+  'number_of_previous_cpp_clean',
+  'uasc_clean',
+  'prop_white_british')
+
+# create table
+baseline_equivalence_int_group <- tableone::CreateTableOne(
+  vars = covariates,
+  strata = "intervention_group", 
+  data = desc_data, test = FALSE)
+
+# extract SMDs
+smd_equivalence_table = print(baseline_equivalence_int_group, smd = TRUE) 
+smd_equivalence_table = as.data.frame(smd_equivalence_table)
+smd_equivalence_table = tibble::rownames_to_column(
+  smd_equivalence_table, "Covariate")
+# looking like really good baseline equivalence
+
+# Save table
+setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
+writexl::write_xlsx(
+  smd_equivalence_table, 
+  'children_with_risk_factors_appendix_table_1_baseline_equivalence.xlsx')
+
+# create plot
+formula = paste0("intervention_group ~ ", paste(covariates, collapse = ' + '))
+
+cobalt::bal.tab(
+  as.formula(formula), 
+  data = desc_data,
+  stats = 'm',
+  binary = 'std',
+  abs = TRUE)
+
+cobalt::love.plot(
+  as.formula(formula), 
+  data = desc_data,
+  stats = 'm',
+  binary = 'std',
+  abs = TRUE)
 
 #### Table 2,3,4,5,6 ----------
 
@@ -412,9 +558,9 @@ appendix_tb_234567 =  desc_data %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
     count = 5*round(`count`/5),
-    count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = 'local_authority', values_from = 'count_percent') %>%
   dplyr::mutate(
@@ -436,7 +582,8 @@ appendix_tb_234567 =  desc_data %>%
 
 # Save table 
 setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
-writexl::write_xlsx(appendix_tb_234567, 'children_with_risk_factors_appendix_table_234567.xlsx')
+writexl::write_xlsx(appendix_tb_1_cont, 'children_with_risk_factors_appendix_table_234567.xlsx')
+
 
 ## 3. Born children only -------------------------------------------------------------------
 
@@ -481,20 +628,28 @@ overall_desc =  desc_data %>%
   describe(class = 'categorical') %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
-    #count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq)
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq)
+
+overall_desc =  desc_data %>% 
+  describe(class = 'numeric') %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq)
 
 ### Table 1 -----------------------------------------------------------------------
 table_1 =  desc_data %>%
   dplyr::group_by(local_authority, intervention_group) %>%
   dplyr::summarise(count =  n()) %>%
-  dplyr::mutate(freq = round(count/sum(count), 2)) %>%
+  dplyr::mutate(freq = round(`count`/sum(`count`), 2)) %>%
   dplyr::mutate(
-    count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
   dplyr::ungroup() %>%
-  dplyr::select(-count, -freq) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = intervention_group, 
     values_from = count_percent
@@ -515,7 +670,7 @@ table_2 =  desc_data %>%
   dplyr::summarise(count =  n()) %>%
   tidyr::pivot_wider(
     names_from = cla_status, 
-    values_from = count
+    values_from = `count`
   ) %>% 
   dplyr::mutate(
     `Total N` = sum(`0`, `1`),
@@ -572,20 +727,25 @@ writexl::write_xlsx(table_3, 'born_only_children_table_3.xlsx')
 
 ### Appendix tables ----
 
-#### Table 1 ----
+#### Table 1.1 baseline cat ----
 
 desc_data %>% 
   dplyr::group_by(local_authority) %>% 
   dplyr::summarise(n())
 
-appendix_tb_1 =  desc_data %>% 
+appendix_tb_1_cat =  desc_data %>% 
   dplyr::group_by(local_authority) %>%
   describe(class = 'categorical', group = c('local_authority')) %>%
+  dplyr::mutate(
+    #count = ifelse(`count` < 5, '[z]', `count`),
+    count = 5*round(`count`/5)
+    ) %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
-    #count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')
+    ) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = 'local_authority', values_from = 'count_percent') %>%
   dplyr::mutate(
@@ -599,9 +759,74 @@ appendix_tb_1 =  desc_data %>%
                 `Swindon (n = 4,338)` = swindon
   )
 
+
 # Save table 
 setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
-writexl::write_xlsx(appendix_tb_1, 'born_only_children_apprendix_table_1.xlsx')
+writexl::write_xlsx(appendix_tb_1_cat, 'born_only_children_appendix_table_1_cat.xlsx')
+
+#### Table 1.2 baseline cont ----
+
+# Numeric vars
+appendix_tb_1_cont =  desc_data %>% 
+  dplyr::select(-age_at_referral_numeric_final) %>%
+  dplyr::group_by(local_authority) %>%
+  describe(class = 'numeric', group = c('local_authority')) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(count_percent_missing = paste0(number_missing, " (", percent_missing, "%)")) %>%
+  dplyr::relocate(c('covariate','local_authority','min', 'max', 'mean', 'sd', 'count_percent_missing')) %>%
+  dplyr::select(-cv,-median,-contains('percentile'), -iqr)
+
+# Save table 
+setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
+writexl::write_xlsx(appendix_tb_1_cont, 'born_only_children_appendix_table_1_cont.xlsx')
+
+#### Table 1.3 baseline equivalence ----
+covariates = c(
+  #'referral_no_further_action_clean',
+  'unborn_flag',
+  'age_at_referral_final',
+  'gender_final',
+  'ethnicity_final',
+  'disability_status_clean',
+  'number_of_previous_cpp_clean',
+  'uasc_clean',
+  'prop_white_british')
+
+# create table
+baseline_equivalence_int_group <- tableone::CreateTableOne(
+  vars = covariates,
+  strata = "intervention_group", 
+  data = desc_data, test = FALSE)
+
+# extract SMDs
+smd_equivalence_table = print(baseline_equivalence_int_group, smd = TRUE) 
+smd_equivalence_table = as.data.frame(smd_equivalence_table)
+smd_equivalence_table = tibble::rownames_to_column(
+  smd_equivalence_table, "Covariate")
+# looking like really good baseline equivalence
+
+# Save table
+setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
+writexl::write_xlsx(
+  smd_equivalence_table, 
+  'born_only_children_appendix_table_1_baseline_equivalence.xlsx')
+
+# create plot
+formula = paste0("intervention_group ~ ", paste(covariates, collapse = ' + '))
+
+cobalt::bal.tab(
+  as.formula(formula), 
+  data = desc_data,
+  stats = 'm',
+  binary = 'std',
+  abs = TRUE)
+
+cobalt::love.plot(
+  as.formula(formula), 
+  data = desc_data,
+  stats = 'm',
+  binary = 'std',
+  abs = TRUE)
 
 #### Table 2,3,4,5,6 ----------
 
@@ -616,9 +841,9 @@ appendix_tb_234567 =  desc_data %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
     count = 5*round(`count`/5),
-    count = ifelse(count < 5, '[z]', count),
-    count_percent = paste0(count,' (', freq*100, '%)')) %>%
-  dplyr::select(-count, -freq) %>%
+    count = ifelse(`count` < 5, '[z]', `count`),
+    count_percent = paste0(`count`,' (', freq*100, '%)')) %>%
+  dplyr::select(-`count`, -freq) %>%
   tidyr::pivot_wider(
     names_from = 'local_authority', values_from = 'count_percent') %>%
   dplyr::mutate(
@@ -640,5 +865,4 @@ appendix_tb_234567 =  desc_data %>%
 
 # Save table 
 setwd(paste0(output_path, '/2. Alternative samples/Report descriptive tables'))
-writexl::write_xlsx(appendix_tb_234567, 'born_only_children_apprendix_table_234567.xlsx')
-
+writexl::write_xlsx(appendix_tb_1_cont, 'born_only_children_appendix_table_234567.xlsx')
